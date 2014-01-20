215ab7e088a4fe87f4c50cfcfa7a0597392ce907
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2014-01-19 19:12:27 -0800

    1.0.0

---
+++ plugin.js
@@ -0,0 +1,78 @@
+module.exports = {
+    init: function (client, imports) {
+        const requiresAdmin = imports.admin.requiresAdmin;
+
+        return {
+            handlers: {
+                '!join': requiresAdmin(function (command) {
+                    if (command.args[0]) {
+                        client.notice(format('PluginControl', 'Joining %s - requested by %s', command.channel, command.prefix));
+                        client.join(command.args.join(' '));
+                    }
+
+                }),
+
+                '!part': requiresAdmin(function (command) {
+                    var channel;
+
+                    if (command.args[0]) {
+                        channel = command.args[0];
+                    } else if (!command.isQuery) {
+                        channel = command.channel;
+                    } else {
+                        return;
+                    }
+
+                    client.notice(format('PluginControl', 'Parting %s - requested by %s', channel, command.prefix));
+                    client.part(channel);
+                }),
+
+                '!quit': requiresAdmin(function (command) {
+                    client.notice(format('PluginControl', 'Quitting network - requested by %s', command.prefix));
+                    client.quit();
+                }),
+
+                '!nick': requiresAdmin(function (command) {
+                    if (command.args[0]) {
+                        client.notice(format('PluginControl', 'Changing nickname to %s - requested by %s', command.args[0], command.prefix));
+                        client.nick(command.args[0]);
+                    } else {
+                        return;
+                    }
+                })
+            },
+
+            help: {
+                'join': [
+                    '!join <channel>',
+                    ' ',
+                    'Join specifed channel.',
+                    'Requires admin privileges.'
+                ],
+                'part': [
+                    '!part [<channel>]',
+                    ' ',
+                    'Part specified channel.',
+                    'If no channel is given, parts channel message was sent in.',
+                    'Requires admin privileges.'
+                ],
+                'quit': [
+                    '!quit [<reason>]',
+                    ' ',
+                    'Quit from network with given reason.',
+                    'Requires admin privileges.'
+                ],
+                'nick': [
+                    '!nick <newnick>',
+                    ' ',
+                    'Change bot\'s nickname to the new nickname.',
+                    'Requires admin privileges.'
+                ]
+            },
+
+            commands: ['join', 'part', 'quit', 'nick']
+        }
+    },
+
+    requiresRoles: ['admin']
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Commands
+
+* !join #chan
+* !part [#chan]
+* !quit
+* !nick new-nick
+
+No message is given on a failed or incorrectly sent command.
