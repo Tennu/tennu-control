2cb7e1108e9785dbcdb9d381abe655c34fbe4223
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-01-16 08:48:28 +0100

    Style: Use double quotes everywhere.

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
-var format = require('util').format;
+var format = require("util").format;
 
 module.exports = {
     init: function (client, imports) {
@@ -6,15 +6,15 @@ module.exports = {
 
         return {
             handlers: {
-                '!join': requiresAdmin(function (command) {
+                "!join": requiresAdmin(function (command) {
                     if (command.args[0]) {
-                        client.notice(format('PluginControl', 'Joining %s - requested by %s', command.channel, command.prefix));
-                        client.join(command.args.join(' '));
+                        client.notice(format("PluginControl", "Joining %s - requested by %s", command.channel, command.prefix));
+                        client.join(command.args.join(" "));
                     }
 
                 }),
 
-                '!part': requiresAdmin(function (command) {
+                "!part": requiresAdmin(function (command) {
                     var channel;
 
                     if (command.args[0]) {
@@ -25,12 +25,12 @@ module.exports = {
                         return;
                     }
 
-                    client.notice(format('PluginControl', 'Parting %s - requested by %s', channel, command.prefix));
+                    client.notice(format("PluginControl", "Parting %s - requested by %s", channel, command.prefix));
                     client.part(channel);
                 }),
 
-                '!quit': requiresAdmin(function (command) {
-                    client.notice(format('PluginControl', 'Quitting network - requested by %s', command.prefix));
+                "!quit": requiresAdmin(function (command) {
+                    client.notice(format("PluginControl", "Quitting network - requested by %s", command.prefix));
 
                     var reason = command.args.join(" ");
                     if (/^\s*$/.test(reason)) {
@@ -40,21 +40,21 @@ module.exports = {
                     client.quit(reason);
                 }),
 
-                '!nick': requiresAdmin(function (command) {
+                "!nick": requiresAdmin(function (command) {
                     if (command.args[0]) {
-                        client.notice(format('PluginControl', 'Changing nickname to %s - requested by %s', command.args[0], command.prefix));
+                        client.notice(format("PluginControl", "Changing nickname to %s - requested by %s", command.args[0], command.prefix));
                         client.nick(command.args[0]);
                     }
                 }),
 
-                '!say': requiresAdmin(function (command) {
+                "!say": requiresAdmin(function (command) {
                     return {
                         target: command.args[0],
                         message: command.args.slice(1).join(" ")
                     };
                 }),
 
-                '!act': requiresAdmin(function (command) {
+                "!act": requiresAdmin(function (command) {
                     return {
                         target: command.args[0],
                         intent: "act",
@@ -62,7 +62,7 @@ module.exports = {
                     };
                 }),
 
-                '!ctcp': requiresAdmin(function (command) {
+                "!ctcp": requiresAdmin(function (command) {
                     return {
                         target: command.args[0],
                         intent: "ctcp",
@@ -72,54 +72,54 @@ module.exports = {
             },
 
             help: {
-                'join': [
-                    '{{!}}join <channel>',
-                    ' ',
-                    'Join specifed channel.',
-                    'Requires admin privileges.'
+                "join": [
+                    "{{!}}join <channel>",
+                    " ",
+                    "Join specifed channel.",
+                    "Requires admin privileges."
                 ],
-                'part': [
-                    '{{!}}part [<channel>]',
-                    ' ',
-                    'Part specified channel.',
-                    'If no channel is given, parts channel message was sent in.',
-                    'Requires admin privileges.'
+                "part": [
+                    "{{!}}part [<channel>]",
+                    " ",
+                    "Part specified channel.",
+                    "If no channel is given, parts channel message was sent in.",
+                    "Requires admin privileges."
                 ],
-                'quit': [
-                    '{{!}}quit [<reason>]',
-                    ' ',
-                    'Quit from network with given reason.',
-                    'Requires admin privileges.'
+                "quit": [
+                    "{{!}}quit [<reason>]",
+                    " ",
+                    "Quit from network with given reason.",
+                    "Requires admin privileges."
                 ],
-                'nick': [
-                    '{{!}}nick <newnick>',
-                    ' ',
-                    'Change bot\'s nickname to the new nickname.',
-                    'Requires admin privileges.'
+                "nick": [
+                    "{{!}}nick <newnick>",
+                    " ",
+                    "Change bot\"s nickname to the new nickname.",
+                    "Requires admin privileges."
                 ],
-                'say': [
-                    '{{!}}say <target> <message>',
-                    ' ',
-                    'Say the message to the channel.',
-                    'Requires admin privileges.'
+                "say": [
+                    "{{!}}say <target> <message>",
+                    " ",
+                    "Say the message to the channel.",
+                    "Requires admin privileges."
                 ],
-                'act': [
-                    '{{!}}act <target> <action>',
-                    ' ',
-                    'Act the action to the channel.',
-                    'Requires admin privileges.'
+                "act": [
+                    "{{!}}act <target> <action>",
+                    " ",
+                    "Act the action to the channel.",
+                    "Requires admin privileges."
                 ],
-                'ctcp': [
-                    '{{!}}ctcp <target> <ctcpType> <ctcpBody>',
-                    ' ',
-                    'Send a CTCP of ctcpType to target with specified body.',
-                    'Requires admin privileges.'
+                "ctcp": [
+                    "{{!}}ctcp <target> <ctcpType> <ctcpBody>",
+                    " ",
+                    "Send a CTCP of ctcpType to target with specified body.",
+                    "Requires admin privileges."
                 ]
             },
 
-            commands: ['join', 'part', 'quit', 'nick', 'say', 'act', 'ctcp']
+            commands: ["join", "part", "quit", "nick", "say", "act", "ctcp"]
         }
     },
 
-    requiresRoles: ['admin']
+    requiresRoles: ["admin"]
 };
